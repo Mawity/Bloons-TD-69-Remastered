36cec49c93b0de5cce427bff1a7cc8c1cb2ca511
Mawity <mateojdenis@gmail.com>
2021-06-08 18:04:44 -0300

    Added Horizontal Movement

--- Bloons TD 69 - Remastered/Assets/Scripts/PlayerController.cs
+++ Bloons TD 69 - Remastered/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 8;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        float h = Input.GetAxis("Horizontal");
+
+        Vector2 pos = transform.position;
+
+        pos.x += h * speed * Time.deltaTime;
+
+        transform.position = pos;
+
+
+
     }
 }
