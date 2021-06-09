7d5e9f4216c6fb218bb144d5b2991bfe5ee96d87
Mawity <mateojdenis@gmail.com>
2021-06-09 03:31:25 -0300

    Update MonkeyInformation.cs

--- Bloons TD 69 - Remastered/Assets/Scripts/MonkeyInformation.cs
+++ Bloons TD 69 - Remastered/Assets/Scripts/MonkeyInformation.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Monkey : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[CreateAssetMenu(fileName = "MonkeyInfo", menuName = "Monkey Information", order = 0)]
+public class MonkeyInformation : ScriptableObject
+{
+
+    [SerializeField] private string _name;
+    [TextArea][SerializeField] private string _description;
+    [SerializeField] private int _cost;
+    [SerializeField] private int _attackSpeed;
+
+    public string Name => _name;
+
+    public string Description => _description;
+
+    public int Cost => _cost;
+
+    public int AttackSpeed => _attackSpeed;
+
+
+
 }
